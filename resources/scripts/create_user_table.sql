2063a5e229673303583972a413bf59c889bbca91
anudeepghosh <anudeepghosh01@gmail.com>
2019-08-27 01:36:18 +0530

    ddl script for USER table modified

--- resources/scripts/create_user_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
